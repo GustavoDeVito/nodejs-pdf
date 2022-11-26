37f3ede73127229e8394f0c4281dad658bd2d820
Gustavo de Vito <gustavodevito38@gmail.com>
2022-11-26 20:05:25 -0300

    .

--- src/models/pdfkit-chart.js
+++ src/models/pdfkit-chart.js
@@ -1,16 +1,40 @@
 const PDFDocument = require('pdfkit')
+const { ChartJSNodeCanvas } = require('chartjs-node-canvas');
 
-function PdfkitChart(data, end) {
-    const doc = new PDFDocument();
+const chart = async () => {
+  const chartJSNodeCanvas = new ChartJSNodeCanvas({ type: 'png', width: 800, height: 600 });
 
-    doc.on('data', data)
-    doc.on('end', end)
-  
-    doc
-      .fontSize(25)
-      .text('Default...', 100, 100);
-  
-    doc.end();
+  const configuration = {
+    type: 'bar',
+    data: {
+      labels: ['Q1', 'Q2', 'Q3', 'Q4'],
+      datasets: [{
+        label: 'Users',
+        data: [50, 60, 70, 180]
+      }]
+    }
+  };
+
+  const imageBuffer = await chartJSNodeCanvas.renderToBuffer(configuration);
+
+  return imageBuffer
+}
+
+async function PdfkitChart(data, end) {
+  const graphic = await chart()
+
+  const doc = new PDFDocument();
+
+  doc.on('data', data)
+  doc.on('end', end)
+
+  doc
+    .fontSize(25)
+    .text('PdfKit...', 100, 100);
+
+  doc.image(graphic)
+
+  doc.end();
 }
 
 module.exports = PdfkitChart;
